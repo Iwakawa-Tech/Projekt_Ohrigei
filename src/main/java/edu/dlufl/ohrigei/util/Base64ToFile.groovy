240ddf9e9fe7e2fd567a21856d49bdd1d05e39c3
YuanYiFei1997 <160450379@student.dlufl.edu.cn>
2019-12-30 17:58:24 +0800

    继续完成图片上传功能，使用ajax+base64的方法

---
+++ src/main/java/edu/dlufl/ohrigei/util/Base64ToFile.groovy
@@ -0,0 +1,11 @@
+package edu.dlufl.ohrigei.util
+
+import sun.misc.BASE64Decoder
+
+import java.security.Timestamp
+
+class Base64ToFile {
+    static boolean base64ToFile(String base64){
+        
+    }
+}
